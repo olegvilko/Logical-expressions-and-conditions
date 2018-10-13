7bec64f222ae1a13ee2e420efd89264e307fd6a5
vilko <helgi8@mail.ru>
2018-10-13 20:59:50 +0300

    Добавьте файлы проекта.

---
+++ Logical expressions and conditions/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_expressions_and_conditions
+{
+    class Program
+    {
+        static bool ShouldFire2(bool enemyInFront, string enemyName, int robotHealth)
+        {
+            return enemyInFront && (enemyName != "boss" || robotHealth >= 50);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ShouldFire2(true, "boss", 50));
+        }
+    }
+}
